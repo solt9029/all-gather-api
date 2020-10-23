04a5c7f69bdc84ab9776d9efba3bab8a9f0ecd1c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-23 15:30:48 +0900

    Add render

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,13 +1,16 @@
 class SchedulesController < ApplicationController
   def create
-    dates = params[:dates].map { |date| date.to_date }
+    schedule = nil
+    current_time = Time.current
+
     ActiveRecord::Base.transaction do
-      current_time = Time.current
       schedule = Schedule.create(title: params[:title], created_at: current_time, updated_at: current_time)
       schedule_dates = params[:dates].map do |date|
         { date: date.to_date, schedule_id: schedule.id, created_at: current_time, updated_at: current_time }
       end
       ScheduleDate.insert_all!(schedule_dates)
     end
+
+    render :json => schedule.to_json
   end
 end
