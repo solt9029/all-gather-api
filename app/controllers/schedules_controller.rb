61ee04c63a9b646ad64ddd5652bc024855e66c28
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-23 15:22:15 +0900

    Add create

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,13 @@
 class SchedulesController < ApplicationController
   def create
-    puts params[:dates].first.to_date
-    puts params[:title]
+    dates = params[:dates].map { |date| date.to_date }
+    ActiveRecord::Base.transaction do
+      current_time = Time.current
+      schedule = Schedule.create(title: params[:title], created_at: current_time, updated_at: current_time)
+      schedule_dates = params[:dates].map do |date|
+        { date: date.to_date, schedule_id: schedule.id, created_at: current_time, updated_at: current_time }
+      end
+      ScheduleDate.insert_all!(schedule_dates)
+    end
   end
 end
