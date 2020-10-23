c0db196df45303a730d3fe752cca765109e51161
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-23 15:46:59 +0900

    Add validation

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,16 +1,20 @@
 class SchedulesController < ApplicationController
   def create
-    schedule = nil
-    current_time = Time.current
+    schedule = Schedule.new(title: params[:title])
 
     ActiveRecord::Base.transaction do
-      schedule = Schedule.create(title: params[:title], created_at: current_time, updated_at: current_time)
-      schedule_dates = params[:dates].map do |date|
-        { date: date.to_date, schedule_id: schedule.id, created_at: current_time, updated_at: current_time }
+      if schedule.save
+        current_time = Time.current
+        schedule_dates = params[:dates].map do |date|
+          { date: date.to_date, schedule_id: schedule.id, created_at: current_time, updated_at: current_time }
+        end
+        ScheduleDate.insert_all!(schedule_dates)
+      else
+        return render json: { message: "Validation Error", errors: schedule.errors.full_messages }, status: 400
       end
-      ScheduleDate.insert_all!(schedule_dates)
     end
-
-    render :json => schedule.to_json
+    render json: schedule.to_json
+  rescue => error
+    render json: { message: "Server Error", errors: [] }, status: 500
   end
 end
