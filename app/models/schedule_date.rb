d0fc06c40f2d45cedf4b96b8f05437d69d485b9e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-31 13:48:23 +0900

    Add association

--- app/models/schedule_date.rb
+++ app/models/schedule_date.rb
@@ -1,4 +1,5 @@
 class ScheduleDate < ApplicationRecord
   belongs_to :schedule
   has_many :schedule_member_schedule_dates
+  has_many :schedule_members, through: :schedule_member_schedule_dates
 end
--- app/models/schedule_member.rb
+++ app/models/schedule_member.rb
@@ -1,3 +1,5 @@
 class ScheduleMember < ApplicationRecord
   belongs_to :schedule
+  has_many :schedule_member_schedule_dates
+  has_many :schedule_dates, through: :schedule_member_schedule_dates
 end
