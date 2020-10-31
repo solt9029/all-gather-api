d74dba06016d5fcbeaad1d178a72b8605cd88c13
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-31 13:55:23 +0900

    Remove date

--- db/migrate/20201011042429_create_schedule_member_schedule_dates.rb
+++ db/migrate/20201011042429_create_schedule_member_schedule_dates.rb
@@ -3,9 +3,8 @@ class CreateScheduleMemberScheduleDates < ActiveRecord::Migration[6.0]
     create_table :schedule_member_schedule_dates, id: :uuid do |t|
       t.belongs_to :schedule_date, null: false, type: :uuid, foreign_key: true, index: true
       t.belongs_to :schedule_member, null: false, type: :uuid, foreign_key: true, index: true
-      t.date :date, null: false
       t.timestamps
     end
-    add_index :schedule_member_schedule_dates, [:schedule_date_id, :schedule_member_id, :date], unique: true, name: 'index_schedule_member_schedule_dates'
+    add_index :schedule_member_schedule_dates, [:schedule_date_id, :schedule_member_id], unique: true, name: 'index_schedule_member_schedule_dates'
   end
 end
