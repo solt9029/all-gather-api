52c1b121ef727d582805047fc2fdb45741da2ad3
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-11-03 18:49:16 +0900

    Add zone

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -23,7 +23,12 @@ class SchedulesController < ApplicationController
       if schedule.save
         current_time = Time.current
         schedule_dates = params[:dates].map do |date|
-          { date: date.to_date, schedule_id: schedule.id, created_at: current_time, updated_at: current_time }
+          { 
+            date: Time.zone.parse(date).to_date,
+            schedule_id: schedule.id, 
+            created_at: current_time, 
+            updated_at: current_time
+          }
         end
         ScheduleDate.insert_all!(schedule_dates)
       else
