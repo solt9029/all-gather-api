dbbaff376da5752e7d907c4a5379c2b0075c8c63
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-11-03 14:04:34 +0900

    Refactor

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,11 +3,5 @@ class Schedule < ApplicationRecord
   has_many :schedule_members
 
   validates :title, presence: true
-  before_validation :set_default_title
-
-  private
-
-  def set_default_title
-    self.title = 'タイトル未設定' if self.title.blank?
-  end
+  before_validation -> { self.title = 'タイトル未設定' }, if: -> { self.title.blank? }
 end
