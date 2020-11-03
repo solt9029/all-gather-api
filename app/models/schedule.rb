f85e0169fb36978d189021fd9037569ac06dab73
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-11-03 14:02:41 +0900

    Add before_validation

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,13 @@
 class Schedule < ApplicationRecord
-  validates :title, presence: true
-  
   has_many :schedule_dates
   has_many :schedule_members
+
+  validates :title, presence: true
+  before_validation :set_default_title
+
+  private
+
+  def set_default_title
+    self.title = 'タイトル未設定' if self.title.blank?
+  end
 end
