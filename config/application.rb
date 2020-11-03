80e4bede3deb62256a109f843048fe7ee4062836
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-11-03 17:57:34 +0900

    Add hosts

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module AllGatherApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.hosts << "allgatherapi.solt9029.com"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
