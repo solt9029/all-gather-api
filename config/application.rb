22d04d724db1c446a8ce628411025789320c08c0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-11-03 18:35:23 +0900

    Add timezone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module AllGatherApi
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.hosts << "allgatherapi.solt9029.com"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
